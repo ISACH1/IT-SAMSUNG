90b31257e90197b497e90194c4810806588c75d9
ISACH1 <91792537+ISACH1@users.noreply.github.com>
2021-10-06 22:20:51 +0300

    Add files via upload

---
+++ Волки и зайцы 1/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int z = 11;
+        int v = 2;
+        int z1 = 0;
+        int s;
+        int z2 = 0;
+        if (N % 2 == 0){
+            for (int n =0; n < N; n = n + 2) {
+            z1 = z * 3 - v * 10;
+            s = z1 - z + z2;
+            if (s >= 70) v = v + s/70;
+            z = z1;
+            z2 = s;
+            }
+        }else {
+            if (N == 1 ) z1 = z * 3;
+            for (int n = 1; n < N ; n = n + 2){
+                z1 = (z * 3 - v * 10);
+                s = z1 - z + z2;
+                z = z1;
+                z2 = s;
+                if (s >= 70) v = v + s/70;
+                z1 = z * 3 ;
+            }
+        }
+        System.out.println( z1 + "Количество зайцев");
+        System.out.println(v  + "Количество волков");
+
+
+
+
+
+    }
+}
