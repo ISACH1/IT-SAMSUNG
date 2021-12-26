e0275c8aa56b88865a119705176322636f653f52
ISACH1 <91792537+ISACH1@users.noreply.github.com>
2021-12-26 15:25:14 +0300

    A

---
+++ PICTURE/app/src/main/java/ru/kirillisachenko/picture/MainActivity.java
@@ -0,0 +1,61 @@
+package ru.kirillisachenko.picture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText et;
+    ProgressBar progressBar;
+    TextView text;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        et = findViewById(R.id.et);
+        progressBar = findViewById(R.id.progress_bar);
+        image = findViewById(R.id.image);
+        button.setOnClickListener(v -> {
+            Thread myThread = new Thread(new MyThread());
+            myThread.start();
+        });
+    }
+
+    class loadImage{
+        public void load() {
+            try {
+                URL url = new URL(et.getText().toString());
+                InputStream inputStream = (InputStream) url.getContent();
+                image.setImageBitmap(BitmapFactory.decodeStream(inputStream));
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private class MyThread implements Runnable{
+        loadImage loadImage = new loadImage();
+        @Override
+        public void run() {
+            loadImage.load();
+        }
+    }
+}
