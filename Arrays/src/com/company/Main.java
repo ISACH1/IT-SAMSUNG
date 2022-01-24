e1f27a3e33414a1633bdc601c13d5e55c711be79
ISACH1 <91792537+ISACH1@users.noreply.github.com>
2022-01-24 12:51:19 -0400

    Add files via upload

---
+++ Arrays/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+
+    public static void main(String[] args) {
+       ArrayList<Integer> A = new ArrayList<>();
+       MyArrayList<Integer> B = new MyArrayList<>();
+       B.add(32);
+       Integer a = 32;
+        System.out.println(B.indexOf(3));
+
+
+    }
+}
